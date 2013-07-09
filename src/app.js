f2de3dfd1f63fd7ed71ace9a5c1bc0cf9e34bb38
Chris Gonzalez <chris.gonzalez@turner.com>
2013-07-09 14:18:13 -0400

    Initial html mock

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').__express);
 app.use(express.static(__dirname + '/public'));
-app.use(express.static("/design", __dirname + '/public'));
+app.use("/design", express.static(__dirname + '/design'));
 
 app.get('/', routes.home);
 app.get('/log', lib.logger.view);
