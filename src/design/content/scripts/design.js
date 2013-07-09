343e002cc9064fca89b9ad46be44cfbe59974beb
Chris Gonzalez <chris.gonzalez@turner.com>
2013-07-09 15:21:16 -0400

    Trying

--- src/design/content/scripts/design.js
+++ src/design/content/scripts/design.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 	
-	$(".list .app *:not(button)").on("touchstart mousedown", function(e){
+	$(".list .app *:not(button)").on("click", function(e){
 		$(".wrapper").addClass("app-view");
 	});
+
+	$(".app .back").on("click", function(e){
+		$(".wrapper").removeClass("app-view");
+	});
+
+	$(".releases .release *:not(button)").on("click", function(e){
+		$(this).parents(".release").toggleClass("active");
+	});	
 
 })
