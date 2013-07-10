033a6c0d648af2111a8972ed1b8591fe1f3361b6
Kevin Brill <kevin.brill@gmail.com>
2013-07-10 10:26:58 -0400

    Home page now returns apps

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,7 +2,7 @@ var database = require('../lib').database;
 
 module.exports = function (request, response)
 {
-	database.find('users', {}, function(err,results){
+	database.find('apps', {}, function(err,results){
 		response.json(200, results);
 	});
 }
