37e4f17cee38c01785e24184a92fae7b9b22b8bb
Kevin Brill <kevin.brill@gmail.com>
2013-07-10 12:48:56 -0400

    Removed logging stuff

--- src/app.js
+++ src/app.js
@@ -10,8 +10,6 @@ var server = http.createServer(app);
 //setup to run on cloud9 or AWS/nginx setup
 var port = process.argv[2] || process.env.PORT;
 
-lib.logger.init(server, true, 100);
-
 server.listen(port);
 
 app.set('view engine', 'ejs');
@@ -21,10 +19,6 @@ app.use(express.static(__dirname + '/public'));
 app.use("/design", express.static(__dirname + '/design'));
 
 app.get('/', routes.home);
-app.get('/log', lib.logger.view);
-app.get('/log/add', lib.logger.add);
-app.get('/log/clear', lib.logger.clear);
-app.get('/log/test', lib.logger.test);
 
 app.get('/api/seed', function(req,res,next){
 
