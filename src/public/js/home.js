9a5b4fb85a172d72e59376288d49fbd82b34c844
Nick Teissler <nicholas.teissler@turner.com>
2013-07-11 11:11:14 -0400

    edit and new version admin buttons

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -9,7 +9,25 @@ $(document).ready(function(){
 
 			$(".wrapper").addClass("dialog");	
 		}
+
 	});
+
+	$(".list .admin").on("click",function(e){
+		var id = $(this).attr('data-id');
+		switch($(e.target).attr("data-action")){
+			
+			case "edit":
+				alert("switch statement");
+				var template = new EJS({url: '/templates/setup.ejs'}).update('dialog', '/api/apps/' + id);
+				$(".wrapper").addClass("dialog");
+				break;
+			case "newVersion":
+				var template = new EJS({url: '/templates/newRelease.ejs'}).update('dialog', '/api/apps/' + id);
+				$(".wrapper").addClass("dialog");
+				break;
+		}
+	});
+
 
 	$("#dialog").on("click", ".close", function(e){
 		$(".wrapper").removeClass("dialog");
